6461b0f59a86209d5603aee8ec2e56e53956c35b
易成杰 <626659321@qq.com>
2023-10-01 13:24:06 +0800

    simple demo

--- hello-r2dbc-mysql/src/test/java/com/yicj/study/r2dbc/ConnectionFactoryTest.java
+++ hello-r2dbc-mysql/src/test/java/com/yicj/study/r2dbc/ConnectionFactoryTest.java
@@ -1,15 +1,14 @@
 package com.yicj.study.r2dbc;
 
 import com.yicj.study.r2dbc.repository.UserEntity;
-import io.asyncer.r2dbc.mysql.MySqlConnectionConfiguration;
-import io.asyncer.r2dbc.mysql.MySqlConnectionFactory;
-import io.r2dbc.spi.Connection;
-import io.r2dbc.spi.ConnectionFactories;
-import io.r2dbc.spi.ConnectionFactory;
-import io.r2dbc.spi.ConnectionFactoryOptions;
+import io.r2dbc.spi.*;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.ZoneId;
 
 import static io.r2dbc.spi.ConnectionFactoryOptions.*;
 
@@ -17,39 +16,44 @@ import static io.r2dbc.spi.ConnectionFactoryOptions.*;
  * @author yicj
  * @date 2023/10/1 11:24
  */
+@Slf4j
 public class ConnectionFactoryTest {
 
     @Test
-    public void hello(){
-        // Notice: the query string must be URL encoded
-        ConnectionFactory connectionFactory = ConnectionFactories.get(
-                ConnectionFactoryOptions.builder()
-                        .option(DRIVER, "mysql")
-                        .option(HOST, "localhost")
-                        .option(PORT, 3306)
-                        .option(USER, "root")
-                        .option(PASSWORD, "root")
-                        .option(DATABASE, "imooc_security")
-                        .build());
-        Mono.from(connectionFactory.create())
-                .flatMapMany(connection ->{
+    public void world() throws InterruptedException {
+        ConnectionFactoryOptions options = ConnectionFactoryOptions.builder()
+                .option(DRIVER, "mysql")
+                //.option(HOST, "192.168.99.51")
+                .option(HOST, "127.0.0.1")
+                .option(PORT, 3306)  // optional, default 3306
+                .option(USER, "root")
+                .option(PASSWORD, "root") // optional, default null, null means has no password
+                .option(DATABASE, "imooc_security") // optional, default null, null means not specifying the database
+                .option(CONNECT_TIMEOUT, Duration.ofSeconds(3)) // optional, default null, null means no timeout
+                .option(Option.valueOf("socketTimeout"), Duration.ofSeconds(4)) // deprecated since 1.0.1, because it has no effect and serves no purpose.
+                .build();
+        ConnectionFactory connectionFactory = ConnectionFactories.get(options);
+
+        // Creating a Mono using Project Reactor
+        Mono<Connection> connectionMono = Mono.from(connectionFactory.create());
+        connectionMono.flatMapMany(connection -> {
                     String sql = "SELECT * FROM audit_log";
                     return Flux.from(connection.createStatement(sql).execute())
                             .flatMap(result ->
-                                result.map((row, metadata) ->{
-                                    Long id = row.get("id", Long.class);
-                                    String name = row.get("name", String.class);
-                                    String username = row.get("username", String.class);
-                                    UserEntity user = new UserEntity() ;
-                                    user.setId(id);
-                                    user.setName(name);
-                                    user.setUsername(username);
-                                    return user ;
-                                })
+                                    result.map((row, metadata) -> {
+                                        Long id = row.get("id", Long.class);
+                                        String name = row.get("name", String.class);
+                                        String username = row.get("username", String.class);
+                                        UserEntity user = new UserEntity();
+                                        user.setId(id);
+                                        user.setName(name);
+                                        user.setUsername(username);
+                                        return user;
+                                    })
                             );
                 })
-                .subscribe(testEntity ->System.out.println(testEntity.toString()));
+                .subscribe(entity -> log.info("entity : {}", entity));
+        Thread.sleep(2000);
     }
-
 
 }
